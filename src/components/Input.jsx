280f255bc27fb793f212c22240c627f99185b14e
Yash Hingane <hingane_y@es.iitr.ac.in>
2023-11-10 17:30:48 +0530

    disbled the button while loading

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,37 +1,42 @@
 import React, { useEffect, useState } from "react";
-import "./../Styles/input.css"
+import "./../Styles/input.css";
 
-function Input({imgArr , setImgArr , currIdx}) {
+function Input({ imgArr, setImgArr, currIdx }) {
   console.log(currIdx);
   const [inputText, setInputText] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  React.useEffect(function(){
-    setInputText(imgArr[currIdx].text)
-  } , [currIdx]);
+  React.useEffect(
+    function () {
+      setInputText(imgArr[currIdx].text);
+    },
+    [currIdx]
+  );
 
   //handling setImgArray
-  function changeState(textInput , imageBlobURL){
-
+  function changeState(textInput, imageBlobURL) {
     //remove prev state
-    setImgArr( imgArr.filter(ele => {
-      return ele.id != currIdx;
-    }));
+    setImgArr(
+      imgArr.filter((ele) => {
+        return ele.id != currIdx;
+      })
+    );
 
     //adding new state
-    setImgArr( (prev) => {
+    setImgArr((prev) => {
       return [
         ...prev,
         {
-          id : currIdx,
+          id: currIdx,
           ImgURL: imageBlobURL,
-          text: textInput
-        }
-      ]
+          text: textInput,
+        },
+      ];
     });
 
     //sorting the array
-    setImgArr((prev) => prev.sort((a , b) => a.id - b.id))
+    setImgArr((prev) => prev.sort((a, b) => a.id - b.id));
   }
 
   console.log(imgArr);
@@ -40,36 +45,41 @@ function Input({imgArr , setImgArr , currIdx}) {
     e.preventDefault(); // Prevent the default form submission
 
     try {
-      const response = await fetch('https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud', {
-        method: 'POST',
-        headers: {
-          'Accept': 'image/png',
-          'Authorization': 'Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM', // Include your Bearer token
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "inputs": inputText }),
-      });
-
+      setIsLoading(true);
+      const response = await fetch(
+        "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud",
+        {
+          method: "POST",
+          headers: {
+            Accept: "image/png",
+            Authorization:
+              "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM", // Include your Bearer token
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ inputs: inputText }),
+        }
+      );
+      setIsLoading(false);
       if (response.ok) {
-
         const imageBlob = await response.blob();
         let imageBlobURL = URL.createObjectURL(imageBlob);
-        changeState(inputText , imageBlobURL);
+        changeState(inputText, imageBlobURL);
 
         console.log(imageBlobURL);
-        setErrorMessage(''); // Clear any previous error message
-
+        setErrorMessage(""); // Clear any previous error message
       } else if (response.status === 401) {
-        setErrorMessage('Authentication failed. Please check your Bearer token.');
-        changeState(inputText , ""); // Clear the image URL
+        setErrorMessage(
+          "Authentication failed. Please check your Bearer token."
+        );
+        changeState(inputText, ""); // Clear the image URL
       } else {
-        setErrorMessage('An error occurred. Please try again later.');
-        changeState(inputText , ""); // Clear the image URL
+        setErrorMessage("An error occurred. Please try again later.");
+        changeState(inputText, ""); // Clear the image URL
       }
     } catch (error) {
-      console.error('Request error:', error);
-      setErrorMessage('An error occurred. Please try again later.');
-      changeState(inputText , "");; // Clear the image URL
+      console.error("Request error:", error);
+      setErrorMessage("An error occurred. Please try again later.");
+      changeState(inputText, ""); // Clear the image URL
     }
   };
 
@@ -82,7 +92,13 @@ function Input({imgArr , setImgArr , currIdx}) {
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
-      <button className="form-button" onClick={handleSubmit}>Search</button>
+      <button
+        className="form-button"
+        onClick={handleSubmit}
+        disabled={isLoading}
+      >
+        Search
+      </button>
     </div>
   );
 }
